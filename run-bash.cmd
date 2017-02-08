63ec14db98f733b0d30766443cea5b3068978a17
Sebastian Krysmanski <noreply@manski.net>
2017-02-08 18:19:00 +0100

    Initial checkin

---
+++ run-bash.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+set TARGET_SCRIPT=run-bash.sh
+
+:: If %GIT_INSTALL_ROOT% is not available, try to guess it.
+if "%GIT_INSTALL_ROOT%"=="" (
+	set GIT_INSTALL_ROOT=%ProgramFiles%\Git
+)
+
+if not exist "%GIT_INSTALL_ROOT%\bin\bash.exe" (
+	echo ERROR: Git for Windows doesn't seem to be installed.
+	echo   You can download it here: https://git-for-windows.github.io/
+	echo   Afterwards, reopen this command window.
+	exit /B 1
+)
+
+:: Make working dir changes local to this script.
+setlocal
+:: Cd to the script directory
+cd %~dp0
+
+:: Important: Don't use "-i" or "Ctrl+C" won't work to cancel SSH password prompts.
+"%GIT_INSTALL_ROOT%\bin\bash.exe" "%TARGET_SCRIPT%" %*
